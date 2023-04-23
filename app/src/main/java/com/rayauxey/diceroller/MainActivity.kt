3d48d3b95a9549f8bc5a20e1abc08d1b001da2d6
RayAuxey <rayauxey@gmail.com>
2023-04-23 16:36:56 +0300

    Initial commit

---
+++ app/src/main/java/com/rayauxey/diceroller/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.rayauxey.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.rayauxey.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
+
+@Composable
+fun DiceWithButtonAndImage() {
+
+}
