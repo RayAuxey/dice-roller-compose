b9097ce683f23fe37334bfcfe436cf67344a68d4
RayAuxey <rayauxey@gmail.com>
2023-04-23 16:53:06 +0300

    Add roll button

--- app/src/main/java/com/rayauxey/diceroller/MainActivity.kt
+++ app/src/main/java/com/rayauxey/diceroller/MainActivity.kt
@@ -3,7 +3,15 @@ package com.rayauxey.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.rayauxey.diceroller.ui.theme.DiceRollerTheme
 
@@ -21,10 +29,21 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage()
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 }
 
 @Composable
-fun DiceWithButtonAndImage() {
-
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.roll))
+        }
+    }
 }
