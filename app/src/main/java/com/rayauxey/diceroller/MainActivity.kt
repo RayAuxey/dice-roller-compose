bdda30482d01b10b8837e8ee43f16627e2b852fb
RayAuxey <rayauxey@gmail.com>
2023-04-23 17:03:03 +0300

    Add dice image

--- app/src/main/java/com/rayauxey/diceroller/MainActivity.kt
+++ app/src/main/java/com/rayauxey/diceroller/MainActivity.kt
@@ -3,16 +3,21 @@ package com.rayauxey.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rayauxey.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +47,8 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = "1")
+        Spacer(modifier = Modifier.padding(16.dp))
         Button(onClick = { /*TODO*/ }) {
             Text(text = stringResource(R.string.roll))
         }
